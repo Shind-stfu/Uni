bb5e85acdcf66f06de82e74f753649de3e6a38c4
Michael <michael.medinam@campusucc.edu.co>
2025-08-25 03:04:25 +0000

    creacion de GestorEstudiantes

---
+++ Tercer semestre/11Agosto/GestorEstudiantes.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// Clase que gestiona una lista de estudiantes
+public class GestorEstudiantes {
+    private ArrayList<Estudiante> estudiantes; // Lista de estudiantes registrados
+
+    // Constructor
+    public GestorEstudiantes() {
+        this.estudiantes = new ArrayList<>();
+    }
+
+    // Método para agregar un estudiante a la lista
+    public void agregarEstudiante(Estudiante e) {
+        estudiantes.add(e);
+    }
+
+    // Método que cuenta cuántos estudiantes hay en cada facultad
+    public void contarPorFacultad() {
+        HashMap<String, Integer> conteo = new HashMap<>();
+
+        // Recorremos todos los estudiantes
+        for (Estudiante e : estudiantes) {
+            String fac = e.getFacultad();
+            conteo.put(fac, conteo.getOrDefault(fac, 0) + 1);
+        }
+
+        // Mostramos el resultado
+        System.out.println("Cantidad de estudiantes por facultad:");
+        for (String facultad : conteo.keySet()) {
+            System.out.println(facultad + ": " + conteo.get(facultad));
+        }
+    }
